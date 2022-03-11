6dbb1b3bbd2a0b7e125789197c6de746a0348f65
Niklus <niklasoti@gmail.com>
2022-03-11 08:41:42 +0100

    Add Service Worker

---
+++ sw.js
@@ -0,0 +1,51 @@
+const cacheName = "cache-v1";
+
+const precacheResources = [
+  "/",
+  "index.html",
+  "script.js",
+  "manifest.json",
+  "192x192.png",
+  "512x512.png",
+  "180x180.png",
+  "storage.js",
+  "styles.css"
+];
+
+self.addEventListener("install", (event) => {
+  console.log("Service worker install event!");
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(precacheResources);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service worker activate event!");
+
+  let cacheKeeplist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
